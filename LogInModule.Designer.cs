b6125ce28024c2b19a47e72a7a6a6e9b727f4c39
Jakub <64312657+ColderVoid@users.noreply.github.com>
2024-10-22 22:24:59 +0200

    Multi Language
    Added template for multi language usage, language folder, example words in strings file

--- LogInModule.Designer.cs
+++ LogInModule.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace NStudio
 {
-    partial class Form1
+    partial class LogInModule
     {
         /// <summary>
         /// Wymagana zmienna projektanta.
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogInModule));
+            this.SuspendLayout();
+            // 
+            // LogInModule
+            // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "LogInModule";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
---
+++ LogInModule.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Resources;
+using System.Reflection;
+using System.Globalization;
+using System.Drawing.Text;
+
+namespace NStudio
+{
+    public partial class LogInModule : Form
+    {
+        private static ResourceManager _rm;
+
+        public LogInModule()
+        {
+
+            InitializeComponent();
+            _rm = new ResourceManager("NStudio.Language.strings", Assembly.GetExecutingAssembly());
+            // usage example by me
+            Console.WriteLine($"{LogInModule.GetString("hello")} {LogInModule.GetString("world")}");
+            LogInModule.ChangeLanguage("en");
+            Console.WriteLine($"{LogInModule.GetString("hello")} {LogInModule.GetString("world")}");
+
+        }
+
+        public static string GetString(string name)
+        {
+            return _rm.GetString(name);
+        }
+
+        public static void ChangeLanguage(string language)
+        {
+
+            var cultureInfo = new CultureInfo(language);
+
+            CultureInfo.CurrentCulture = cultureInfo;
+            CultureInfo.CurrentUICulture = cultureInfo;
+
+        }
+    }
+}
