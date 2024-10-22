03ab5e07b60bc4739cea274d282cf7c4d53adfcf
Jakub <64312657+ColderVoid@users.noreply.github.com>
2024-10-23 00:24:06 +0200

    LogIn Page [23102024]
    Started coding login page/login module

--- LogInModule.Designer.cs
+++ LogInModule.Designer.cs
@@ -29,18 +29,129 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogInModule));
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // logo
+            // 
+            resources.ApplyResources(this.logo, "logo");
+            this.logo.BackColor = System.Drawing.Color.Transparent;
+            this.logo.Name = "logo";
+            this.logo.TabStop = false;
+            // 
+            // loginLabel
+            // 
+            resources.ApplyResources(this.loginLabel, "loginLabel");
+            this.loginLabel.BackColor = System.Drawing.Color.Transparent;
+            this.loginLabel.Cursor = System.Windows.Forms.Cursors.Default;
+            this.loginLabel.ForeColor = System.Drawing.Color.Black;
+            this.loginLabel.Name = "loginLabel";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Name = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Name = "label2";
+            // 
+            // username
+            // 
+            this.username.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.username.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.username, "username");
+            this.username.ForeColor = System.Drawing.Color.Black;
+            this.username.Name = "username";
+            // 
+            // password
+            // 
+            this.password.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.password.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.password, "password");
+            this.password.ForeColor = System.Drawing.Color.Black;
+            this.password.Name = "password";
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Name = "panel1";
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Name = "panel2";
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
             // LogInModule
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.loginLabel);
+            this.Controls.Add(this.logo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "LogInModule";
+            this.Load += new System.EventHandler(this.LogInModule_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox logo;
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
--- LogInModule.cs
+++ LogInModule.cs
@@ -8,10 +8,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// LanguageModule
 using System.Resources;
 using System.Reflection;
 using System.Globalization;
 using System.Drawing.Text;
+using System.Drawing.Drawing2D;
+
+// TODO COLDERVOID
+// remember me checkbox
+// reset password label button
+// summit button for login
+//login module
+// startup module
+
 
 namespace NStudio
 {
@@ -31,6 +41,30 @@ namespace NStudio
 
         }
 
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            // Ustawienie kolorów gradientu
+            Color color1 = ColorTranslator.FromHtml("#833ab4");  // Pierwszy kolor
+            Color color2 = ColorTranslator.FromHtml("#fd1d1d");  // Drugi kolor
+            Color color3 = ColorTranslator.FromHtml("#fcb045");  // Trzeci kolor
+
+            // Utworzenie pędzla gradientowego
+            using (LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle, color1, color2, 45F))
+            {
+                // Dodanie trzeciego koloru w gradient
+                ColorBlend colorBlend = new ColorBlend();
+                colorBlend.Colors = new Color[] { color1, color2, color3 };
+                colorBlend.Positions = new float[] { 0f, 0.5f, 1f };
+
+                brush.InterpolationColors = colorBlend;
+
+                // Wypełnienie tła formularza gradientem
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+
+            base.OnPaint(e);
+        }
+
         public static string GetString(string name)
         {
             return _rm.GetString(name);
@@ -43,6 +77,21 @@ namespace NStudio
 
             CultureInfo.CurrentCulture = cultureInfo;
             CultureInfo.CurrentUICulture = cultureInfo;
+
+        }
+
+        private void LogInModule_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
 
         }
     }
