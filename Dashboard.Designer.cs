d789f2ec0c0e72c8106879540c56398a2de78285
Jakub <64312657+ColderVoid@users.noreply.github.com>
2024-10-30 21:23:40 +0100

    Dashboard[0]
    UI Layout

--- Dashboard.Designer.cs
+++ Dashboard.Designer.cs
@@ -28,19 +28,249 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.settingsButton = new FontAwesome.Sharp.IconButton();
+            this.panelMenuBuffor = new System.Windows.Forms.Panel();
+            this.shopButton = new FontAwesome.Sharp.IconButton();
+            this.artistsButton = new FontAwesome.Sharp.IconButton();
+            this.recordsButton = new FontAwesome.Sharp.IconButton();
+            this.songsButton = new FontAwesome.Sharp.IconButton();
+            this.panelDashboard = new System.Windows.Forms.Panel();
+            this.lblLogo = new System.Windows.Forms.Label();
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.panelTitle = new System.Windows.Forms.Panel();
+            this.panelTopBuffor = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.panelDashboard.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.panelMenu.Controls.Add(this.settingsButton);
+            this.panelMenu.Controls.Add(this.panelMenuBuffor);
+            this.panelMenu.Controls.Add(this.shopButton);
+            this.panelMenu.Controls.Add(this.artistsButton);
+            this.panelMenu.Controls.Add(this.recordsButton);
+            this.panelMenu.Controls.Add(this.songsButton);
+            this.panelMenu.Controls.Add(this.panelDashboard);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(220, 561);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // settingsButton
+            // 
+            this.settingsButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.settingsButton.FlatAppearance.BorderSize = 0;
+            this.settingsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.settingsButton.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.settingsButton.ForeColor = System.Drawing.Color.White;
+            this.settingsButton.IconChar = FontAwesome.Sharp.IconChar.Cog;
+            this.settingsButton.IconColor = System.Drawing.Color.White;
+            this.settingsButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.settingsButton.IconSize = 30;
+            this.settingsButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.settingsButton.Location = new System.Drawing.Point(0, 420);
+            this.settingsButton.Name = "settingsButton";
+            this.settingsButton.Padding = new System.Windows.Forms.Padding(15, 0, 20, 0);
+            this.settingsButton.Size = new System.Drawing.Size(220, 60);
+            this.settingsButton.TabIndex = 5;
+            this.settingsButton.Text = "settingsButton";
+            this.settingsButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.settingsButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.settingsButton.UseVisualStyleBackColor = true;
+            this.settingsButton.Click += new System.EventHandler(this.settingsButton_Click);
+            // 
+            // panelMenuBuffor
+            // 
+            this.panelMenuBuffor.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelMenuBuffor.Location = new System.Drawing.Point(0, 360);
+            this.panelMenuBuffor.Name = "panelMenuBuffor";
+            this.panelMenuBuffor.Size = new System.Drawing.Size(220, 60);
+            this.panelMenuBuffor.TabIndex = 6;
+            // 
+            // shopButton
+            // 
+            this.shopButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.shopButton.FlatAppearance.BorderSize = 0;
+            this.shopButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.shopButton.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.shopButton.ForeColor = System.Drawing.Color.White;
+            this.shopButton.IconChar = FontAwesome.Sharp.IconChar.StoreAlt;
+            this.shopButton.IconColor = System.Drawing.Color.White;
+            this.shopButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.shopButton.IconSize = 30;
+            this.shopButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.shopButton.Location = new System.Drawing.Point(0, 300);
+            this.shopButton.Name = "shopButton";
+            this.shopButton.Padding = new System.Windows.Forms.Padding(15, 0, 20, 0);
+            this.shopButton.Size = new System.Drawing.Size(220, 60);
+            this.shopButton.TabIndex = 4;
+            this.shopButton.Text = "shopButton";
+            this.shopButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.shopButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.shopButton.UseVisualStyleBackColor = true;
+            this.shopButton.Click += new System.EventHandler(this.shopButton_Click);
+            // 
+            // artistsButton
+            // 
+            this.artistsButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.artistsButton.FlatAppearance.BorderSize = 0;
+            this.artistsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.artistsButton.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.artistsButton.ForeColor = System.Drawing.Color.White;
+            this.artistsButton.IconChar = FontAwesome.Sharp.IconChar.Star;
+            this.artistsButton.IconColor = System.Drawing.Color.White;
+            this.artistsButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.artistsButton.IconSize = 30;
+            this.artistsButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.artistsButton.Location = new System.Drawing.Point(0, 240);
+            this.artistsButton.Name = "artistsButton";
+            this.artistsButton.Padding = new System.Windows.Forms.Padding(15, 0, 20, 0);
+            this.artistsButton.Size = new System.Drawing.Size(220, 60);
+            this.artistsButton.TabIndex = 3;
+            this.artistsButton.Text = "artistsButton";
+            this.artistsButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.artistsButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.artistsButton.UseVisualStyleBackColor = true;
+            this.artistsButton.Click += new System.EventHandler(this.artistsButton_Click);
+            // 
+            // recordsButton
+            // 
+            this.recordsButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.recordsButton.FlatAppearance.BorderSize = 0;
+            this.recordsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.recordsButton.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.recordsButton.ForeColor = System.Drawing.Color.White;
+            this.recordsButton.IconChar = FontAwesome.Sharp.IconChar.RecordVinyl;
+            this.recordsButton.IconColor = System.Drawing.Color.White;
+            this.recordsButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.recordsButton.IconSize = 30;
+            this.recordsButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.recordsButton.Location = new System.Drawing.Point(0, 180);
+            this.recordsButton.Name = "recordsButton";
+            this.recordsButton.Padding = new System.Windows.Forms.Padding(15, 0, 20, 0);
+            this.recordsButton.Size = new System.Drawing.Size(220, 60);
+            this.recordsButton.TabIndex = 2;
+            this.recordsButton.Text = "recordsButton";
+            this.recordsButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.recordsButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.recordsButton.UseVisualStyleBackColor = true;
+            this.recordsButton.Click += new System.EventHandler(this.recordsButton_Click);
+            // 
+            // songsButton
+            // 
+            this.songsButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.songsButton.FlatAppearance.BorderSize = 0;
+            this.songsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.songsButton.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.songsButton.ForeColor = System.Drawing.Color.White;
+            this.songsButton.IconChar = FontAwesome.Sharp.IconChar.Music;
+            this.songsButton.IconColor = System.Drawing.Color.White;
+            this.songsButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.songsButton.IconSize = 30;
+            this.songsButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.songsButton.Location = new System.Drawing.Point(0, 120);
+            this.songsButton.Name = "songsButton";
+            this.songsButton.Padding = new System.Windows.Forms.Padding(15, 0, 20, 0);
+            this.songsButton.Size = new System.Drawing.Size(220, 60);
+            this.songsButton.TabIndex = 1;
+            this.songsButton.Text = "songsButton";
+            this.songsButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.songsButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.songsButton.UseVisualStyleBackColor = true;
+            this.songsButton.Click += new System.EventHandler(this.songsButton_Click);
+            // 
+            // panelDashboard
+            // 
+            this.panelDashboard.Controls.Add(this.lblLogo);
+            this.panelDashboard.Controls.Add(this.logo);
+            this.panelDashboard.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelDashboard.Location = new System.Drawing.Point(0, 0);
+            this.panelDashboard.Name = "panelDashboard";
+            this.panelDashboard.Size = new System.Drawing.Size(220, 120);
+            this.panelDashboard.TabIndex = 0;
+            // 
+            // lblLogo
+            // 
+            this.lblLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblLogo.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblLogo.ForeColor = System.Drawing.Color.White;
+            this.lblLogo.Location = new System.Drawing.Point(0, 62);
+            this.lblLogo.Name = "lblLogo";
+            this.lblLogo.Size = new System.Drawing.Size(220, 24);
+            this.lblLogo.TabIndex = 1;
+            this.lblLogo.Text = "NStudio";
+            this.lblLogo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblLogo.Click += new System.EventHandler(this.lblLogo_Click);
+            // 
+            // logo
+            // 
+            this.logo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.logo.Image = ((System.Drawing.Image)(resources.GetObject("logo.Image")));
+            this.logo.Location = new System.Drawing.Point(0, 0);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(220, 62);
+            this.logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.logo.TabIndex = 0;
+            this.logo.TabStop = false;
+            this.logo.Click += new System.EventHandler(this.logo_Click);
+            // 
+            // panelTitle
+            // 
+            this.panelTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.panelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitle.Location = new System.Drawing.Point(220, 0);
+            this.panelTitle.Name = "panelTitle";
+            this.panelTitle.Size = new System.Drawing.Size(764, 65);
+            this.panelTitle.TabIndex = 1;
+            // 
+            // panelTopBuffor
+            // 
+            this.panelTopBuffor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.panelTopBuffor.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTopBuffor.Location = new System.Drawing.Point(220, 65);
+            this.panelTopBuffor.Name = "panelTopBuffor";
+            this.panelTopBuffor.Size = new System.Drawing.Size(764, 7);
+            this.panelTopBuffor.TabIndex = 2;
             // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(189, 126);
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.panelTopBuffor);
+            this.Controls.Add(this.panelTitle);
+            this.Controls.Add(this.panelMenu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(1000, 600);
             this.Name = "Dashboard";
             this.Text = "Dashboard";
+            this.Load += new System.EventHandler(this.Dashboard_Load);
+            this.panelMenu.ResumeLayout(false);
+            this.panelDashboard.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panelDashboard;
+        private FontAwesome.Sharp.IconButton songsButton;
+        private FontAwesome.Sharp.IconButton shopButton;
+        private FontAwesome.Sharp.IconButton artistsButton;
+        private FontAwesome.Sharp.IconButton recordsButton;
+        private FontAwesome.Sharp.IconButton settingsButton;
+        private System.Windows.Forms.Panel panelTitle;
+        private System.Windows.Forms.Panel panelTopBuffor;
+        private System.Windows.Forms.Panel panelMenuBuffor;
+        private System.Windows.Forms.PictureBox logo;
+        private System.Windows.Forms.Label lblLogo;
     }
 }
--- Dashboard.cs
+++ Dashboard.cs
@@ -3,24 +3,126 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FontAwesome.Sharp;
 
 namespace NStudio
 {
     public partial class Dashboard : Form
     {
         private DatabaseControl dbControl;
+        private IconButton currentBtn;
+        private Panel leftBorderBtn;
+        private struct RGBColors
+        {
+
+            public static readonly Color songsColor = Color.FromArgb(158, 205, 250);
+            public static readonly Color recordsColor = Color.FromArgb(131, 247, 215);
+            public static readonly Color artistsColor = Color.FromArgb(147, 157, 254);
+            public static readonly Color shopColor = Color.FromArgb(134, 125, 253);
+            public static readonly Color settingsColor = Color.FromArgb(237, 28, 36);
+
+        }
         public Dashboard(DatabaseControl dbControl)
         {
             InitializeComponent();
             this.dbControl = dbControl;
+            leftBorderBtn = new Panel();
+            leftBorderBtn.Size = new Size(7, 60);
+            panelMenu.Controls.Add(leftBorderBtn);
 
             //string cs = dbControl.connectionString;
             //string cs = dbControl.SendConnectionString();
             //Console.WriteLine(cs);
+        }
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+            songsButton.Text = LogInModule.GetString("songsButton");
+            recordsButton.Text = LogInModule.GetString("recordsButton");
+            artistsButton.Text = LogInModule.GetString("artistsButton");
+            shopButton.Text = LogInModule.GetString("shopButton");
+            settingsButton.Text = LogInModule.GetString("settingsButton");
+        }
+
+        private void ActivateButton(object sender, Color color)
+        {
+            if (sender != null)
+            {
+                DeactivateButton();
+                currentBtn = (IconButton)sender;
+                currentBtn.BackColor = Color.FromArgb(31, 31, 31);
+                currentBtn.ForeColor = color;
+                currentBtn.IconColor = color;
+                currentBtn.TextAlign = ContentAlignment.MiddleCenter;
+                currentBtn.TextImageRelation = TextImageRelation.ImageBeforeText;
+                currentBtn.ImageAlign = ContentAlignment.MiddleRight;
+
+                leftBorderBtn.BackColor = color;
+                leftBorderBtn.Location = new Point(0, currentBtn.Location.Y);
+                leftBorderBtn.Visible = true;
+                leftBorderBtn.BringToFront();
+            }
+        }
+
+        private void DeactivateButton()
+        {
+            if (currentBtn != null)
+            {
+                currentBtn.BackColor = Color.FromArgb(41, 41, 41);
+                currentBtn.ForeColor = Color.White;
+                currentBtn.IconColor = Color.White;
+                currentBtn.TextAlign = ContentAlignment.MiddleLeft;
+                currentBtn.TextImageRelation = TextImageRelation.ImageBeforeText;
+                currentBtn.ImageAlign = ContentAlignment.MiddleLeft;
+            }
+        }
+
+        private void Reset()
+        {
+
+            DeactivateButton();
+            leftBorderBtn.Visible = false;
+
+        }
+
+        private void songsButton_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.songsColor);
+        }
+
+        private void recordsButton_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.recordsColor);
+        }
+
+        private void artistsButton_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.artistsColor);
+        }
+
+        private void shopButton_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.shopColor);
+        }
+
+        private void settingsButton_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.settingsColor);
+        }
+
+        private void logo_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void lblLogo_Click(object sender, EventArgs e)
+        {
+            Reset();
         }
     }
 }
